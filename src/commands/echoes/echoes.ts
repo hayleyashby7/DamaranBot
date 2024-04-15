1e81db8052ac8c3181dd182b62f7d5f0e7c4a1f3
Hayley Ashby <hayley@laptop.com>
2024-04-15 20:13:32 +0100

    echo used command

--- src/commands/echoes/echoes.ts
+++ src/commands/echoes/echoes.ts
@@ -2,28 +2,68 @@ import { CommandInteraction, ApplicationCommandType } from 'discord.js';
 import { Command } from '../../types/Command';
 import { SupabaseClient } from '@supabase/supabase-js';
 
+const ECHOES_LEFT = 'Echoes Left';
+
 export const EchoesLeft = (dbClient: SupabaseClient): Command => ({
     name: 'echoes-left',
     description: 'Number of Echoes left',
     type: ApplicationCommandType.ChatInput,
     run: async (interaction: CommandInteraction) => {
-        const content = await getEchoesLeft(dbClient);
-
-        await interaction.followUp({
-            ephemeral: true,
-            content,
-        });
+        try {
+            const content = await getEchoesLeft(dbClient);
+            await interaction.followUp({
+                ephemeral: true,
+                content,
+            });
+        } catch (error) {
+            console.error(error);
+            await interaction.followUp({
+                ephemeral: true,
+                content: 'An error occurred while getting the number of echoes left.',
+            });
+        }
     },
 });
 
-const getEchoesLeft = async (dbClient: SupabaseClient): Promise<string | undefined> => {
-    try {
-        const { data } = await dbClient.from('stats').select('name, value').eq('name', 'Echoes Left');
-        if (data && data.length > 0) {
-            const { value } = data[0];
-            return value.toString();
+export const EchoUsed = (dbClient: SupabaseClient): Command => ({
+    name: 'echo-used',
+    description: 'Updates number of echoes used',
+    type: ApplicationCommandType.ChatInput,
+    run: async (interaction: CommandInteraction) => {
+        try {
+            const content = await setEchoUsed(dbClient);
+            await interaction.followUp({
+                ephemeral: true,
+                content,
+            });
+        } catch (error) {
+            console.error(error);
+            await interaction.followUp({
+                ephemeral: true,
+                content: 'An error occurred while updating the number of echoes used.',
+            });
         }
-    } catch (error) {
-        console.error(error);
+    },
+});
+
+const getEchoesLeft = async (dbClient: SupabaseClient): Promise<string> => {
+    const { data } = await dbClient.from('stats').select('name, value').eq('name', ECHOES_LEFT);
+    if (data && data.length > 0) {
+        const { value } = data[0];
+        return value.toString();
     }
+    throw new Error('No data');
+};
+
+const setEchoUsed = async (dbClient: SupabaseClient): Promise<string> => {
+    const currentEchoes = Number(await getEchoesLeft(dbClient));
+    const updatedEchoes = currentEchoes - 1;
+
+    const { data } = await dbClient.from('stats').update({ value: updatedEchoes }).eq('name', ECHOES_LEFT).select();
+
+    if (data && data.length > 0) {
+        const { value } = data[0];
+        return `Echoes now remaining: ${value}`;
+    }
+    throw new Error('Unable to update echoes');
 };
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,7 @@
 import { Command } from '../types/Command';
 import { Ping } from './ping/ping';
 import { Campaign } from './campaign/campaign';
-import { EchoesLeft } from './echoes/echoes';
+import { EchoesLeft, EchoUsed } from './echoes/echoes';
 import { dbClient } from '../services/dbClient';
 
-export const Commands: Command[] = [Ping, Campaign, EchoesLeft(dbClient)];
+export const Commands: Command[] = [Ping, Campaign, EchoesLeft(dbClient), EchoUsed(dbClient)];
