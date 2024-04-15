52ab356f646be8aa602b8d8efd88f94cb1abf1a8
Hayley Ashby <hayley@laptop.com>
2024-04-15 19:18:58 +0100

    added echo test

--- src/commands/echoes/echoes.test.ts
+++ src/commands/echoes/echoes.test.ts
@@ -1,6 +1,7 @@
 import { expect, describe, it, vi, beforeEach } from 'vitest';
 import { EchoesLeft } from './echoes';
 import { CommandInteraction } from 'discord.js';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 vi.mock('discord.js', async (importOriginal) => {
     const mod = await importOriginal<typeof import('discord.js')>();
@@ -10,17 +11,33 @@ vi.mock('discord.js', async (importOriginal) => {
     };
 });
 
+vi.mock('@supabase/supabase-js', async (importOriginal) => {
+    const mod = await importOriginal<typeof import('@supabase/supabase-js')>();
+    return {
+        ...mod,
+        SupabaseClient: vi.fn(),
+    };
+});
+
 let interaction: CommandInteraction;
+let mockDbClient: SupabaseClient;
 
 beforeEach(() => {
     interaction = {
         followUp: vi.fn(),
     } as unknown as CommandInteraction;
+
+    mockDbClient = {
+        from: vi.fn().mockReturnThis(),
+        select: vi.fn().mockReturnThis(),
+        eq: vi.fn().mockResolvedValue({ data: [{ value: '123' }] }),
+    } as unknown as SupabaseClient;
 });
 
 describe('echoes-left command', () => {
     it('should reply with echoes left', async () => {
-        await EchoesLeft.run(interaction);
+        const command = EchoesLeft(mockDbClient);
+        await command.run(interaction);
         expect(interaction.followUp).toHaveBeenCalledWith({
             ephemeral: true,
             content: '123',
