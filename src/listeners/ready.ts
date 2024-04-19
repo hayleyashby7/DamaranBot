c6987ee7b725f813c31eab8b0fa4427b6d486d7f
Hayley Ashby <hayley@laptop.com>
2024-04-19 14:22:44 +0100

    debugging

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -4,7 +4,7 @@ import { Commands } from '../commands';
 export default (client: Client): void => {
     client.once('ready', async () => {
         await client.application?.commands.set(Commands);
-
+        console.log(process.env.NODE_ENV);
         console.log('Journey to Damaran bleep bloop 🤖');
     });
 };
